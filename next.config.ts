d34f38bdf5ad873bfa23d9229ef28ca7dbf5208f
Rachelejr <rachelejr779@gmail.com>
2026-06-04 23:57:25 -0400

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,9 @@
-﻿import type { NextConfig } from 'next';
+import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
-  turbopack: undefined,
+  experimental: {
+    turbo: undefined,
+  },
 };
 
 export default nextConfig;
